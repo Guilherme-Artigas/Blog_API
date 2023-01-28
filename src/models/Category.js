1fae95c2246d9ff31409f43d17785bf5dd5b54c5
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-01-28 11:28:45 -0300

    feat: requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+      tablename: 'categories',
+      underscored: true,
+    },
+  );
+  return Category;
+};
