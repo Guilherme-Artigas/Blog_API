d172c53221382d8b95d83b66c429e2a1792937f1
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-01-29 08:33:55 -0300

    feat: requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        field: 'post_id',
+        type: DataTypes.INTEGER,
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        field: 'category_id',
+        type: DataTypes.INTEGER,
+      }
+    },
+    {
+      timestamps: false,
+      tablename: 'posts_categories',
+      underscored: true,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(
+      models.BlogPost,
+      {
+        as: 'blogposts',
+        through: PostCategory,
+        foreignKey: 'id',
+        otherKey: 'id',
+      },
+    );
+
+    models.BlogPost.belongsToMany(
+      models.Category,
+      {
+        as: 'categories',
+        through: PostCategory,
+        foreignKey: 'id',
+        otherKey: 'id',
+      }
+    )
+  };
+
+  return PostCategory;
+};
